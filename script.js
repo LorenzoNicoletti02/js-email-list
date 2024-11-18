53f00d14647264da6159494f4aa37cadd34b4927
LorenzoNicoletti02 <lorenzo.nicoletti02@gmail.com>
2024-11-18 16:31:01 +0100

    feat; first setting

---
+++ script.js
@@ -0,0 +1,12 @@
+const email = axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((resp) => {
+    const email = resp.data.response;
+});
+
+const emails = [];
+console.log(email);
+
+
+for (i = email; i < email; i ++) {
+    const element = emails[i];
+    console.log();
+}
